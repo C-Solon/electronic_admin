ff608298c405f1bc52e2c618ca5f787badfd6338
Raghu Betina <raghu@firstdraft.com>
2019-08-27 23:52:00 +0000

    Whitelist columns in Charge for ActiveAdmin

--- app/admin/charges.rb
+++ app/admin/charges.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Charge do
+
+ permit_params :amount, :paid, :appointment_id, :treatment_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
