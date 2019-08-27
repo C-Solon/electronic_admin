5b6cbd0c377b61df3a8060dbbbac3ff519647367
Raghu Betina <raghu@firstdraft.com>
2019-08-27 23:52:11 +0000

    Whitelist columns in WorkRelationship for ActiveAdmin

--- app/admin/work_relationships.rb
+++ app/admin/work_relationships.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register WorkRelationship do
+
+ permit_params :professional_id, :office_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
