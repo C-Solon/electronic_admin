f3e40f29e02862b5dd64e41ef3753c0587c91aee
Raghu Betina <raghu@firstdraft.com>
2019-08-27 23:51:42 +0000

    Whitelist columns in Professional for ActiveAdmin

--- app/admin/professionals.rb
+++ app/admin/professionals.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Professional do
+
+ permit_params :first_name, :last_name, :phone_number, :picture, :email, :password
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
