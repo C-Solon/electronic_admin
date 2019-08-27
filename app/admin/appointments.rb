4bf080603bec8da8b9cf9c85a5e62dcb012dae12
Raghu Betina <raghu@firstdraft.com>
2019-08-27 23:51:56 +0000

    Whitelist columns in Appointment for ActiveAdmin

--- app/admin/appointments.rb
+++ app/admin/appointments.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Appointment do
+
+ permit_params :office_id, :patient_id, :professional_id, :charge_id, :notes, :time_slot
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
